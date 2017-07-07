817089e61e57229e6fd100ba16c7ebe14205931e
Roman TSovanyan <covrom@gmail.com>
2017-07-07 16:44:36 +0300

    tokens and opers

--- gonec.go
+++ gonec.go
@@ -30,11 +30,9 @@ func Interpreter() *interpreter {
 // тело запроса - это код для интерпретации
 func (i *interpreter) handlerMain(w http.ResponseWriter, r *http.Request) {
 
-	var overconn bool
-
 	i.RLock()
 	//лимит количества одновременных подключений к одному интерпретатору
-	overconn = len(i.clientConnections) > 2
+	overconn := len(i.clientConnections) >= 2
 	i.RUnlock()
 
 	if overconn {
@@ -47,20 +45,25 @@ func (i *interpreter) handlerMain(w http.ResponseWriter, r *http.Request) {
 	}
 
 	i.Lock()
-	numtodel := len(i.clientConnections)
 	i.clientConnections = append(i.clientConnections, clconn)
 	i.Unlock()
 
-	defer func(n int) {
+	defer func(cc clientConnection) {
 		i.Lock()
-		i.clientConnections = append(i.clientConnections[:n], i.clientConnections[n+1:]...)
+		for n := range i.clientConnections {
+			if i.clientConnections[n] == cc {
+				i.clientConnections = append(i.clientConnections[:n], i.clientConnections[n+1:]...)
+				break
+			}
+		}
 		i.Unlock()
-	}(numtodel)
+	}(clconn)
 
 	if r.ContentLength > 1<<26 {
 		http.Error(w, "Слишком большой запрос", http.StatusForbidden)
 		return
 	}
+
 	switch r.Method {
 
 	case http.MethodPost:
@@ -99,4 +102,155 @@ func (i *interpreter) ParseAndRun(r io.Reader, w io.Writer) (err error) {
 	//TODO: синхронно запускается код модуля, но он может создавать вэб-сервера и горутины, которые будут работать и после возврата
 
 	return nil
+}
+
+// Приведенные далее ключевые слова являются зарезервированными и не могут использоваться в качестве создаваемых имен переменных, реквизитов объектов конфигурации и объявляемых процедур и функций.
+// В данном варианте языка каждое из ключевых слов имеет два представления – русское и английское.
+const (
+	iEOF = -(iota + 1)
+	rIf
+	rThen
+	rElsIf
+	rElse
+	rEndIf
+	rFor
+	rEach
+	rIn
+	rTo
+	rWhile
+	rDo
+	rEndDo
+	rProcedure
+	rFunction
+	rEndProcedure
+	rEndFunction
+	rVar
+	rGoto
+	rReturn
+	rContinue
+	rBreak
+	rAnd
+	rOr
+	rNot
+	rTry
+	rExcept
+	rRaise
+	rEndTry
+	rNew
+	rExecute
+
+	oComment
+	oLineFeed
+	oLabelStart
+	oLabelEnd
+	oEOp
+	oLBr
+	oRBr
+	oLSqBr
+	oRSqBr
+	oComma
+	oDQuote
+	oSQuote
+	oPoint
+	oAdd
+	oSub
+	oMul
+	oDiv
+	oRemDiv
+	oGt
+	oGe
+	oLt
+	oLe
+	oEq
+	oNe
+)
+
+var tokenMap = map[string]rune{
+	"Если":      rIf,
+	"If":        rIf,
+	"Тогда":     rThen,
+	"Then":      rThen,
+	"ИначеЕсли": rElsIf,
+	"ElsIf":     rElsIf,
+	"Иначе":     rElse,
+	"Else":      rElse,
+	"КонецЕсли": rEndIf,
+	"EndIf":     rEndIf,
+	"Для":       rFor,
+	"For":       rFor,
+	"Каждого":   rEach,
+	"Each":      rEach,
+	"Из":        rIn,
+	"In":        rIn,
+	"По":        rTo,
+	"To":        rTo,
+	"Пока":      rWhile,
+	"While":     rWhile,
+	"Цикл":      rDo,
+	"Do":        rDo,
+	"КонецЦикла":     rEndDo,
+	"EndDo":          rEndDo,
+	"Процедура":      rProcedure,
+	"Procedure":      rProcedure,
+	"Функция":        rFunction,
+	"Function":       rFunction,
+	"КонецПроцедуры": rEndProcedure,
+	"EndProcedure":   rEndProcedure,
+	"КонецФункции":   rEndFunction,
+	"EndFunction":    rEndFunction,
+	"Перем":          rVar,
+	"Var":            rVar,
+	"Перейти":        rGoto,
+	"Goto":           rGoto,
+	"Возврат":        rReturn,
+	"Return":         rReturn,
+	"Продолжить":     rContinue,
+	"Continue":       rContinue,
+	"Прервать":       rBreak,
+	"Break":          rBreak,
+	"И":              rAnd,
+	"And":            rAnd,
+	"Или":            rOr,
+	"Or":             rOr,
+	"Не":             rNot,
+	"Not":            rNot,
+	"Попытка":        rTry,
+	"Try":            rTry,
+	"Исключение": rExcept,
+	"Except":     rExcept,
+	"ВызватьИсключение": rRaise,
+	"Raise":        rRaise,
+	"КонецПопытки": rEndTry,
+	"EndTry":       rEndTry,
+	"Новый":        rNew,
+	"New":          rNew,
+	"Выполнить":    rExecute,
+	"Execute":      rExecute,
+}
+
+var operMap = map[string]rune{
+	"//": oComment,    //Двумя знаками косая черта начинается комментарий. Комментарием считается весь текст от символа до конца текущей строки
+	"|":  oLineFeed,   //Используется только в строковых константах в начале строки и означает, что данная строка является продолжением предыдущей (перенос строки)
+	"~":  oLabelStart, //Начало метки оператора
+	":":  oLabelEnd,   //Окончание метки оператора
+	";":  oEOp,        //Символ разделения операторов
+	"(":  oLBr,
+	")":  oRBr, //В круглые скобки заключается список параметров методов, процедур, функций и конструкторов. Также они используются в выражениях встроенного языка
+	"[":  oLSqBr,
+	"]":  oRSqBr,  //С помощью оператора квадратные скобки производится обращение к свойствам объекта по строковому представлению имени свойства.Также возможно обращение к элементам коллекций по индексу или другому параметру
+	",":  oComma,  //Разделяет параметры в списке параметров методов, процедур, функций и конструкторов
+	"\"": oDQuote, //Обрамляет строковые литералы
+	"'":  oSQuote, //Обрамляет литералы даты
+	".":  oPoint,  //Десятичная точка в числовых литералах. Разделитель, используемый для обращения к свойствам и методам объектов встроенного языка
+	"+":  oAdd,    //Операция сложения. Операция конкатенации строк
+	"-":  oSub,    //Операция вычитания
+	"*":  oMul,    //Операция умножения
+	"/":  oDiv,    //Операция деления
+	"%":  oRemDiv, //Получение остатка от деления. Допускается использование дробных значений делимого и делителя
+	">":  oGt,     //Логическая операция Больше
+	">=": oGe,     //Логическая операция Больше или равно
+	"<":  oLt,     //Логическая операция Меньше
+	"<=": oLe,     //Логическая операция Меньше или равно
+	"=":  oEq,     //Операция присваивания. Логическая операция Равно
+	"<>": oNe,     //Логическая операция Не равно
 }
